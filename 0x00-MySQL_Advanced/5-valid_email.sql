849dc0b65e0ca23dc8533b26cbf2b3e2209d1b28
Eliud254 <eliudobure@gmail.com>
2024-05-09 18:38:42 +0000

    Email validation

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,10 @@
+-- To validation to send an email
+DELIMITER //
+CREATE TRIGGER reset_email BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+   IF NEW.email <> OLD.email THEN SET NEW.valid_email = 0;
+   END IF;
+END;//
+DELIMITER ;
+
