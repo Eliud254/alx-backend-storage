abdc1472657a2ce63ddac719761e9b6b4d710852
Eliud254 <eliudobure@gmail.com>
2024-05-15 20:10:17 +0000

    Correction

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,46 +1,45 @@
 #!/usr/bin/env python3
-# Implementing an expiring web cache and tracker
+'''Implementing an expiring web cache and tracker'''
+
 import redis
 import requests
 from datetime import timedelta
 
 
 def get_page(url: str) -> str:
-    # Retrieves the content of a URL, tracks the number of accesses,
-    # and caches the result with an expiration time of 10 seconds.
+    '''Retrieves the content of a URL, tracks the number of accesses,
+    and caches the result with an expiration time of 10 seconds.
 
-    # Check if the URL is valid
+    Args:
+        url (str): The URL to fetch content from.
+
+    Returns:
+        str: The content of the URL.
+    '''
+
     if url is None or len(url.strip()) == 0:
         return ''
 
-    # Connect to Redis
     redis_store = redis.Redis()
 
-    # Define Redis keys for result and request count
     res_key = 'result:{}'.format(url)
     req_key = 'count:{}'.format(url)
 
-    # Check if the result is cached in Redis
     result = redis_store.get(res_key)
     if result is not None:
-        # If cached, increment request count and return result
         redis_store.incr(req_key)
         return result.decode('utf-8')
 
-    # If not cached, fetch the content from the URL
     result = requests.get(url).content.decode('utf-8')
 
-    # Cache the result in Redis with an expiration time of 10 seconds
     redis_store.setex(res_key, timedelta(seconds=10), result)
-
-    # Increment request count
     redis_store.incr(req_key)
 
     return result
 
 
 if __name__ == "__main__":
-    # Example usage
     url = "http://slowwly.robertomurray.co.uk"
     print(get_page(url))
+
 
